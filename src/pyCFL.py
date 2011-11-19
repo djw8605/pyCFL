14f45fce081d315137e39868396514880ed2cf11
Derek Weitzel <djw8605@gmail.com>
2011-11-18 22:22:41 -0600

    Finished up parsing rules.

--- src/pyCFL.py
+++ src/pyCFL.py
@@ -4,18 +4,34 @@ import sys, re
 
 def CreatePDA(rules):
     rule_regex = re.compile("([A-Z])->([\w]*)")
-    substition_regex = re.compile("[A-Z]")
+    substition_regex = re.compile("([A-Z])")
+    variables = {}
     
     for rule in rules:
         rule_object = rule_regex.search(rule)
         if rule_object is None:
             print "Error parsing rule: " + rule
         (variable, transform) = rule_object.groups()
+        
         constants = substition_regex.split(transform)
         
+        # Remove blank entries
+        while '' in constants:
+            constants.remove('')
         
-        print transform
-        print constants
+        if not variables.has_key(variable):
+            variables[variable] = []
+        variables[variable].append(constants)
+        
+        
+        
+    return variables
+
+
+def TestLine(parsed_rules, line):
+    pass
+    
+    
 
 
 def main():
@@ -26,10 +42,10 @@ def main():
     for i in range(number_of_rules):
         rules.append(sys.stdin.readline().strip())
         
-    CreatePDA(rules)
-    print rules
+    parsed_rules = CreatePDA(rules)
     
-    
+    for line in sys.stdin.readlines():
+        TestLine(parsed_rules, line)
     
 
 
